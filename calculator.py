65cc985ddfbd5d03d5dc5ed780d72c3a0388feb8
Fayajur Rohman Fayaj <Fayajurrohmanfayaj@gmail.com>
2022-12-04 21:16:32 +0600

    comment has benn added for yourreadability

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
-print()
+
+print() # This i sjust for dacurations...
+
+# this variable is contained a & b's value ..
+# where you can change the value 
 a = 20
 b = 10
 
+# this logic contained the main opaation in this program..
 print("Sum of your number is : ", a+b)
 print("Subtraction of your number is : ", a-b)
 print("Multiplication of your number is : ", a*b)
