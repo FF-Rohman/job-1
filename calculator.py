b004c6c5dc7a784ce9b3e731fc1652d83312b33c
Fayajur Rohman Fayaj <Fayajurrohmanfayaj@gmail.com>
2022-12-04 19:07:59 +0600

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print()
+_1st_num= int(input('Enter Your First Number: '))
+_2nd_num = int(input('Enter Your First Number: '))
+
+print('Addition of those numbe is : ',(_1st_num + _2nd_num))
+print('Subtraction those numbe is : ',(_1st_num - _2nd_num))
+print('Multiplication of those numbe is : ',(_1st_num * _2nd_num))
+print('Multiplication of those numbe is : ',(_1st_num / _2nd_num))
